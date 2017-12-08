b7260c9c2ef951ca83d48c12318717c48ae54909
TChydro <33527113+TChydro@users.noreply.github.com>
2017-12-08 15:28:36 -0600

    Wasn't getting the correct path for public files.
    Should work correctly now, please update your pages if they were not redirecting right before.

--- sessionHeader.php
+++ sessionHeader.php
@@ -3,11 +3,17 @@
 session_start()  ;
 $dir = realpath(__DIR__ . '/../..').'/Private/PHP' ;
 $pdir = dirname(__FILE__) ;
+$temp[] = preg_split("[/]" , $pdir) ;
+$pubDir = "";
+for($i = 3 ; $i < sizeof($temp[0]) ; $i++)
+{
+	$pubDir = $pubDir . "/" . $temp[0][$i] ;
+}
 require_once($dir.'/initialize.php') ;
 global $db ;
 if(!isset($_SESSION['ID']))
 	{
-		header("Location:" . $pdir . "/Login.php") ;
+		header("Location:" . $pubDir . "/Login.php") ;
 	}
 	else
 	{
