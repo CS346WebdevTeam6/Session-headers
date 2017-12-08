e3015fea1bbb0697651183a06ccbb9855f260b44
TChydro <33527113+TChydro@users.noreply.github.com>
2017-12-08 09:30:01 -0600

    fixed a letter

--- sessionHeader.php
+++ sessionHeader.php
@@ -7,7 +7,7 @@ require_once($dir.'/initialize.php') ;
 global $db ;
 if(!isset($_SESSION['ID']))
 	{
-		header("Location:" . $pdir . "/login.php") ;
+		header("Location:" . $pdir . "/Login.php") ;
 	}
 	else
 	{
