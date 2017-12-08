e1ea661c902eab86c58cd095e4f8ba2e70ae0cc3
TChydro <33527113+TChydro@users.noreply.github.com>
2017-12-08 09:21:38 -0600

    Create sessionHeader.php

---
+++ sessionHeader.php
@@ -0,0 +1,21 @@
+//The session start needs to be on the first line of PHP
+<?php
+session_start()  ;
+$dir = '/var/www/students/team6/CS346TeamWebsite/Private/PHP' ;
+$pdir = '/students/team6/CS346TeamWebsite/Public/PHP' ;
+require_once($dir.'/initialize.php') ;
+global $db ;
+if(!isset($_SESSION['ID']))
+	{
+		header("Location:" . $pdir . "/login.php") ;
+	}
+	else
+	{
+		$UN = $_SESSION['username'] ;
+		$id = $_SESSION['ID'] ;
+		$role = $_SESSION['role'] ;
+	}
+?>
+
+//Make sure your page has this meta tag in the head section
+<meta name="viewport" content="width=device-width, initial-scale=1" />
